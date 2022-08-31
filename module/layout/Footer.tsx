888361a7084c31d38dc17f1399547ccefb949641
Hieu Nguyen T. [5] VN.Danang <hieu.nguyen5@monstar-lab.com>
2022-08-31 17:24:53 +0700

    add missing button at fotter

--- module/layout/Footer.tsx
+++ module/layout/Footer.tsx
@@ -1,35 +1,43 @@
 import React, { useState } from "react";
 import { InView } from "react-intersection-observer";
-
+import CustomButton from "../element/CustomButton";
 export default function Footer() {
   const [isView, setView] = useState(false);
   return (
     <footer className="footer">
       <div className="footer-top">
-        <h1>
-          Create a website that{" "}
-          <em className="hightlight-header">
-            <strong> stands out</strong>
-            <em className={`basic-underline ${isView && "animated"}`}>
-              {" "}
-              <svg
-                viewBox="-320 -70.8161 640.4 59.82"
-                preserveAspectRatio="none"
-              >
-                <path
-                  style={{ animationDuration: "1.8s" }}
-                  d="M-300,-56 C-50,-72 298,-65 300,-59 C332,-53 -239,-36 -255,-27 C-271,-18 -88,-24 91,-20"
-                  stroke="#fff"
-                  pathLength="1"
-                  strokeWidth="4.4444444444444"
-                  fill="none"
-                ></path>
-              </svg>
-            </em>
-          </em>{" "}
-        </h1>
-        <InView as="div" triggerOnce  onChange={(inView) => setView(inView)}></InView>
+        <div className="tittle">
+          {" "}
+          <h1>
+            Create a website that{" "}
+            <em className="hightlight-header">
+              <strong> stands out</strong>
+              <em className={`basic-underline ${isView && "animated"}`}>
+                {" "}
+                <svg
+                  viewBox="-320 -70.8161 640.4 59.82"
+                  preserveAspectRatio="none"
+                >
+                  <path
+                    style={{ animationDuration: "1.8s" }}
+                    d="M-300,-56 C-50,-72 298,-65 300,-59 C332,-53 -239,-36 -255,-27 C-271,-18 -88,-24 91,-20"
+                    stroke="#fff"
+                    pathLength="1"
+                    strokeWidth="4.4444444444444"
+                    fill="none"
+                  ></path>
+                </svg>
+              </em>
+            </em>{" "}
+          </h1>
+        <CustomButton onClick={() => ""}>Purchase Salient</CustomButton>
+        </div>
 
+        <InView
+          as="div"
+          triggerOnce
+          onChange={(inView) => setView(inView)}
+        ></InView>
       </div>
       <div className="footer-bottom">
         <p>© 2022 Salient.</p>
